a73e1276f37f4ecc3649440887dce70d61288773
David <dcs1460@gmail.com>
2025-05-12 00:57:49 +0200

    feat: Transform the function to compute the closest vector using the babai algorithm in a method of a class. This class also implements a method to compute the distance between the vectors `v` and `w`

--- babai/babai.py
+++ babai/babai.py
@@ -5,16 +5,45 @@ necessary to execute the Babai algorithm.
 
 import numpy as np
 
+from babai.exceptions import ClosestVectorHasNotBeenComputed
 
-def babai_algorithm(base: np.array, w: np.array) -> np.array:
+
+class BabaiAlgorithm():
     """
-    Implementation of the babai algorithm. This function
-    recieves a base `B` and an arbitrary vector `w`. Each
-    column of `B` must represent a vector of the basis.
-    It returns a vector `v` close to `w`.
+    This class implements the Babai algorithm.
+    It also allows to compute the distance between
+    the two vectors and decide if it's a good basis.
     """
-    assert base.shape[0] == w.shape[0]
-    t = np.linalg.solve(base, w)
-    a = np.round(t)
-    v = base @ a
-    return v
+    def __init__(self, base: np.array, w: np.array):
+        assert base.shape[0] == w.shape[0]
+        self.base = base
+        self.w = w
+        self.v: np.array | None = None
+
+    def compute_closest_vector(self) -> None:
+        """
+        Implementation of the babai algorithm. This function
+        uses the base `B` and the arbitrary vector `w`. Each
+        column of `B` must represent a vector of the basis.
+        It stores the vector `v` close to `w`.
+        """
+        t = np.linalg.solve(self.base, self.w)
+        a = np.round(t)
+        v = self.base @ a
+        self.v = v
+
+    def compute_distance(self) -> float:
+        """
+        If the closest vector `v` has been computed, this
+        function returns the distance between the vector
+        `v` and the vector `w`.
+        """
+        if self.v is None:
+            raise ClosestVectorHasNotBeenComputed
+        return np.linalg.norm(self.w - self.v)
+
+    def is_a_good_base() -> bool:
+        """
+        Returns `True` if the base `B` can be
+        considered a good base. Returns `False` otherwise.
+        """
